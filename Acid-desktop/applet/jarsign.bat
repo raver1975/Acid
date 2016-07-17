51557a98effa7cbc0133bef3a06b4c61d3bee014
Paul Klemstine <paul.klemstine@liveality.com>
2016-07-17 01:35:12 -0500

    changes

--- Acid-desktop/applet/jarsign.bat
+++
@@ -1,7 +0,0 @@
-
-"C:\Program Files\Java\jdk1.7.0_40\bin\jarsigner.exe" -keystore LudumDareKeystore.ks -storepass %1 Acid.jar raver1975 
-"C:\Program Files\Java\jdk1.7.0_40\bin\jarsigner.exe" -keystore LudumDareKeystore.ks -storepass %1 gdx-backend-lwjgl-natives.jar raver1975 
-"C:\Program Files\Java\jdk1.7.0_40\bin\jarsigner.exe" -keystore LudumDareKeystore.ks -storepass %1 gdx-backend-lwjgl.jar raver1975 
-"C:\Program Files\Java\jdk1.7.0_40\bin\jarsigner.exe" -keystore LudumDareKeystore.ks -storepass %1 gdx-natives.jar raver1975 
-"C:\Program Files\Java\jdk1.7.0_40\bin\jarsigner.exe" -keystore LudumDareKeystore.ks -storepass %1 gdx.jar raver1975 
-"C:\Program Files\Java\jdk1.7.0_40\bin\jarsigner.exe" -keystore LudumDareKeystore.ks -storepass %1 lwjgl_util_applet.jar raver1975 
