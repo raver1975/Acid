538ed80ba9074c1c06d62ea7d87ffa56115e2766
Paul Klemstine <paulklemstine@yahoo.com>
2017-11-21 11:15:19 -0600

    Update Acid.java

--- core/src/com/acid/Acid.java
+++ core/src/com/acid/Acid.java
@@ -828,7 +828,7 @@ public class Acid implements ApplicationListener {
         pauseButton.setPosition(100f, 95);
         pauseButton.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if (!pauseButton.isChecked()) Output.pause();
+                if (pauseButton.isChecked()) Output.pause();
                 else Output.resume();
                 pauseButton.setColor(pauseButton.isChecked() ? Color.RED : Color.WHITE);
                 return true;
